946fea24960ccd90941ed7fc9eb583cf8cf2fdd1
Megha-BR <67511600+Megha-BR@users.noreply.github.com>
2020-08-05 20:09:35 +0530

    change environment variable for deployment on vercel

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -14,25 +14,8 @@ export default ({note}) => {
   )
 }
 
-/*export async function getServerSideProps({params, req, res}){
-    const response = await fetch(`http://localhost:3000/api/note/${params.id}`);
-    
-
-    if(!response.ok) {
-        res.writeHead(302, {Location: "/notes"});
-        res.end();
-        return {props: {}};
-    }
-
-    const {data} = await response.json();
-    if(data){
-        return {
-            props:{note:data}
-        }
-    }
-}*/
 export async function getServerSideProps({params, req, res}) {
-    const response = await fetch(`http://localhost:3000/api/note/${params.id}`)
+    const response = await fetch(`${process.env.VERCEL_URL}/api/note/${params.id}`)
   
     // so much power!
     if (!response.ok) {
